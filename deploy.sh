6428fb507c89428adf3821c12dfcba812194a0ad
Win <wocwin@163.com>
2020-06-11 14:48:24 +0800

    ci:新增配置文件

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/heye1109/blog.git master:build-pages
+git push -f git@github.com:wocwin/my-vuePress.git master:build-pages
 
 
 cd -
